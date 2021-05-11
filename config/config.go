f9501bbf51f769b79ef027a2ce36f956f65215ab
RyazanovAlexander <alexander.ryazanov.1987@gmail.com>
2021-05-11 17:04:12 +0300

    changed proto protocol

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
+/*
 MIT License
 
-Copyright (c) 2021 RyazanovAlexander
+Copyright The pipeline-manager Authors.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -19,3 +20,36 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+*/
+
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+// Config is global object that holds all application level variables.
+var Config appConfig
+
+type appConfig struct {
+	Debug bool `mapstructure:"DEBUG"`
+
+	ExecutorName   string `mapstructure:"NAME"`
+	ServerGrpcPort string `mapstructure:"SERVER_GRPC_PORT"`
+}
+
+// Load loads config from environment variables
+func Load() error {
+	viper.AutomaticEnv()
+	viper.SetEnvPrefix("COMMAND_EXECUTOR")
+
+	viper.BindEnv("DEBUG")
+	viper.BindEnv("NAME")
+	viper.BindEnv("SERVER_GRPC_PORT")
+
+	if err := viper.Unmarshal(&Config); err != nil {
+		return err
+	}
+
+	return nil
+}
